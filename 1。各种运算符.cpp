a1d886919fbd79a930fedbb046d1cfff13fbbfa5
HaoXuefeng <46835743+HaoXuefeng@users.noreply.github.com>
2020-08-26 09:22:05 +0800

    Add files via upload

---
+++ 1。各种运算符.cpp
@@ -0,0 +1,64 @@
+
+# include <iostream>
+using namespace std;
+
+//		赋值运算符：		=赋值，+=，-=，*=，/=，%=
+/*
+int main() {
+
+	int a;
+	a = 10;
+	cout << "a = " << a << endl;
+	a += 2;
+	cout << "a += 2,a = " << a << endl;
+	a = 10;
+	a -= 2;
+	cout << "a -= 2,a = " << a << endl;
+	a = 10;
+	a *= 2;
+	cout << "a *= 2,a = " << a << endl;
+	a = 10;
+	a /= 2;
+	cout << "a /= 2,a = " << a << endl;
+	a = 10;
+	a %= 3;
+	cout << "a %= 3,a = " << a << endl;
+
+	system("pause");
+	return 0;
+}
+*/
+
+//		比较运算符		>，<, <=, >=, ==, !=
+/*
+int main()
+{
+	int a = 10, b = 20;
+	cout << "a = " << a << "\t" << "b = " << b << endl;
+	cout << "a == b 返回：" << (a == b) << endl;				//	注意！ 输出表达式的话要加括号！否则报错！
+	cout << "a != b 返回：" << (a != b) << endl;
+	cout << "a >= b 返回：" << (a >= b) << endl;
+	cout << "a <= b 返回：" << (a <= b) << endl;
+	cout << "a > b 返回：" << (a > b) << endl;
+	cout << "a < b 返回：" << (a < b) << endl;
+
+
+	system("pause");
+	return 0;
+}
+*/
+
+
+//		逻辑运算符		!非， &&与， ||或
+/*
+int main()
+{
+	int a = 10, b = 0;
+	cout << !a << endl;
+	cout << (a && b) << endl;		//	注意！	这里也要用上括号！
+	cout << (a || b) << endl;
+
+	system("pause");
+	return 0;
+}
+*/
